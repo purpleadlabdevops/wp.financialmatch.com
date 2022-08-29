ee41091ef17d5f135b65edfadc8d720a3b7914ae
Max Globa <onyx18121990@gmail.com>
2022-08-29 13:42:58 +0300

    changed db creds

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'financialmatch' );
+define( 'DB_NAME', 'dfmyphecgg' );
 
 /** Database username */
-define( 'DB_USER', 'financialmatch' );
+define( 'DB_USER', 'dfmyphecgg' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'financialmatch123456' );
+define( 'DB_PASSWORD', '7eS3nn3NSg' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
